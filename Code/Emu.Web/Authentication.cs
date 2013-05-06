bbbc5bbd0132b901c737386a27987bed358fb389
Brian Newton <bnewton@unomaha.edu>
2013-05-06 13:10:17 -0500

    Trying to deploy

---
+++ Code/Emu.Web/Authentication.cs
@@ -0,0 +1,53 @@
+﻿using Emu.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Emu.Web
+{
+    public static class Authentication
+    {
+        static bool requireAuthentication = false;
+
+        public static User CurrentUser
+        {
+            get { return HttpContext.Current.Session[ "UserInfo" ] as User; }
+            set { HttpContext.Current.Session[ "UserInfo" ] = value; }
+        }
+
+        public static bool IsAuthenticated
+        {
+            get { return requireAuthentication == false || CurrentUser != null; }
+        }
+
+        public static bool IsAdmin
+        {
+            get
+            {
+                return requireAuthentication == false 
+                    || (IsAuthenticated
+                    && CurrentUser.UserType == UserType.Admin);
+            }
+        }
+
+        public static User AuthenticateUser( string username, string password )
+        {
+            using( var db = new EmuDb() )
+            {
+                var result = db.Users.SingleOrDefault( user => user.UserName == username && user.Password == password );
+                if( result != null )
+                {
+                    CurrentUser = result;
+                }
+                return result;
+            }
+        }
+
+        public static void LogOut()
+        {
+            CurrentUser = null;
+        }
+
+    }
+}
---
+++ Code/Emu.Web/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Emu.Web.Models
+{
+    public class LoginModel
+    {
+        [Required(AllowEmptyStrings=false, ErrorMessage = "Please enter in a username to log in")]
+        public string Username { get; set; }
+        [Required(AllowEmptyStrings=false, ErrorMessage = "Please enter a password to log in")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
