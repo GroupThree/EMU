e8a104d4331a9c0fe2bd8acbedac2ed3ec5fc6d5
Ajay Medury <abmedury@unomaha.edu>
2013-05-06 09:53:38 -0500

    Added bootstrap styling to the project again

--- Code/Emu.Web/Global.asax.cs
+++ Code/Emu.Web/Global.asax.cs
@@ -22,7 +22,7 @@ namespace Emu.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
+            //AuthConfig.RegisterAuth();
         }
     }
 }
